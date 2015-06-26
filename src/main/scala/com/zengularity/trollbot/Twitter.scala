658382c14ed0618ae223dad38f4da48c72a56170
Nizar Sehli <nse@zenexity.com>
2015-06-26 15:29:02 +0200

    Make it compile

--- src/main/scala/com/zengularity/trollbot/Twitter.scala
+++ src/main/scala/com/zengularity/trollbot/Twitter.scala
@@ -6,7 +6,7 @@ import twitter4j.{Twitter, TwitterFactory}
 import twitter4j.auth.AccessToken
 import twitter4j.conf.ConfigurationBuilder
 
-case class HashTag(name: String)
+case class Hashtag(val name: String) extends AnyVal
 
 case class Tweet(
   author: String,
@@ -18,18 +18,20 @@ case class Tweet(
 }
 
 object TwitterApp {
+  val conf: Config = ConfigFactory.load();
   val APP_KEY: String             = conf.getString("appKey")
   val ACCESS_TOKEN_SECRET: String = conf.getString("accessTokenSecret")
   val APP_SECRET: String          = conf.getString("appSecret")
   val ACCESS_TOKEN: String        = conf.getString("accessToken")
 
   lazy val client: Twitter = {
-    val conf: Config = ConfigFactory.load();
 
-    new TwitterFactory(new ConfigurationBuilder().build())
-        .getInstance()
-        .setOAuthConsumer(TwitterApp.APP_KEY, TwitterApp.APP_SECRET)
-        .setOAuthAccessToken(
-          new AccessToken(TwitterApp.ACCESS_TOKEN, TwitterApp.ACCESS_TOKEN_SECRET))
+    val factory = new TwitterFactory(new ConfigurationBuilder().build())
+    val t = factory.getInstance()
+    t.setOAuthConsumer(TwitterApp.APP_KEY, TwitterApp.APP_SECRET)
+
+    t.setOAuthAccessToken(new AccessToken(TwitterApp.ACCESS_TOKEN, TwitterApp.ACCESS_TOKEN_SECRET))
+
+    t
   }
 }
