2f8330debfbd34ec893e90c6ea922ffa18bf5695
Vincent Debergue <vde@zengularity.com>
2015-06-26 12:38:21 +0200

    request

--- src/main/scala/com/zengularity/trollbot/Main.scala
+++ src/main/scala/com/zengularity/trollbot/Main.scala
@@ -3,6 +3,11 @@ package com.zengularity.trollbot
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.actor._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl._
+
+import scala.concurrent.duration._
+import scala.concurrent._
 
 object Main {
   def main(args: Array[String]) {
@@ -11,7 +16,23 @@ object Main {
     implicit val system = ActorSystem("reactive-tweets")
     implicit val mat = ActorMaterializer()
 
-    val tweets: Source[Tweet, Unit] = ???
+    // val tweets: Source[Tweet, Unit] = ???
+
+    val connectionFlow = Http().outgoingConnectionTls("stream.twitter.com")
+    val baseRequest = HttpRequest(method= HttpMethods.POST, uri = "/1.1/statuses/filter.json")
+    val signedRequest = sign(baseRequest)
+    val response = Source.single(HttpRequest(method= HttpMethods.POST, uri = "/1.1/statuses/filter.json"))
+      .via(connectionFlow)
+      .runWith(Sink.head)
+
+    val res = Await.result(response, 1 second)
+    println(res)
+
+    system.shutdown()
+  }
+
+  def signedRequest(req: HttpRequest): HttpRequest = {
+
   }
 
 }
