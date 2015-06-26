f2d1819950a9950ba811af39c8036b4bee649ff9
Vincent Debergue <vde@zengularity.com>
2015-06-26 10:12:09 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := """trollbot"""
+
+version := "0.1"
+
+libraryDependencies ++= Seq(
+
+)
+
---
+++ src/main/scala/com/zengularity/trollbot/Main.scala
@@ -0,0 +1,7 @@
+package com.zengularity.trollbot
+
+object Main {
+  def main(args: Array[String]) {
+    println("Hello trollers")
+  }
+}
