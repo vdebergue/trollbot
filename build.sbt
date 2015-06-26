f2b4308f6c04cbd3f297f9fe6cda31a77a01c833
Vincent Debergue <vde@zengularity.com>
2015-06-26 10:42:57 +0200

    add akka streaming

--- build.sbt
+++ build.sbt
@@ -2,7 +2,9 @@ name := """trollbot"""
 
 version := "0.1"
 
-libraryDependencies ++= Seq(
+scalaVersion := "2.11.7"
 
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http-experimental" % "1.0-RC4"
 )
 
--- src/main/scala/com/zengularity/trollbot/Main.scala
+++ src/main/scala/com/zengularity/trollbot/Main.scala
@@ -1,7 +1,19 @@
 package com.zengularity.trollbot
 
+import akka.stream._
+import akka.stream.scaladsl._
+import akka.actor._
+
 object Main {
   def main(args: Array[String]) {
-    println("Hello trollers")
+    println("-- Hello trollers --")
+
+    implicit val system = ActorSystem("reactive-tweets")
+    implicit val mat = ActorMaterializer()
+
+    val tweets: Source[Tweet, Unit] = ???
   }
+
 }
+
+case class Tweet(content: String)
